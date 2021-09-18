56e640fa84aead50f1562de5a0ed424e90cd28f1
Your Name <you@example.com>
2021-09-18 19:06:55 +0300

    update release

--- check4fsm/ProcessAppeal.py
+++ check4fsm/ProcessAppeal.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python
+import os
+
+import csv
+import json
+# import subprocess
+#
+# import requests
+from loguru import logger
+from natasha import (
+    Segmenter,
+    MorphVocab,
+
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    NewsNERTagger,
+
+    PER,
+    NamesExtractor,
+
+    Doc, ORG, LOC
+)
 
 class ProcessAppeal:
+    @logger.catch
     def __init__(self):
-        
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+
+        self.emb = NewsEmbedding()
+        self.ner_tagger = NewsNERTagger(self.emb)
+        self.morph_tagger = NewsMorphTagger(self.emb)
+        self.syntax_parser = NewsSyntaxParser(self.emb)
+        self.names_extractor = NamesExtractor(self.morph_vocab)
+
+
+
+    def __call__(self):
+        pass
