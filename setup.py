7c9ed57c4a15d34ad139764bae88103d0ddd8592
Your Name <you@example.com>
2021-09-18 21:18:10 +0300

    Bump version: 0.0.3 → 0.0.4-dev0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = parse_requirements()
 
 setuptools.setup(
     name='check4fsm',
-    version='0.0.3',
+    version='0.0.4',
     # scripts=['arcturus'],
     author="Stanisalv Kiselev",
     author_email="ristleell@gmail.com",
